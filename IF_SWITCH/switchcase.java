f5c79f4b61fd71dc7c31b4a88e848ed628a8c4cd
NguyenThanhTin23 <thanhtin243@gmail.com>
2024-12-24 10:22:39 +0700

    Ngay 24/12/24

---
+++ IF_SWITCH/switchcase.java
@@ -0,0 +1,15 @@
+package IF_SWITCH;
+
+public class switchcase {
+     public static void main(String[] args) {
+          int day = 2;
+          switch (day) {
+               case 4: {
+                    System.out.println("Thurday");
+                    break;
+               }
+               default:
+                    System.out.println("khong hop le");
+          }
+     }
+}
